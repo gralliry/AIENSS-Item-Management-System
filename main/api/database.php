a27f86f6169c2b588c2e12f6fd995a44437330a3
aiccy <aiccyxixy@163.com>
2024-08-24 18:07:23 +0800

    2.2.0 通过系统变量获取配置

--- main/api/database.php
+++ main/api/database.php
@@ -1,54 +1,70 @@
 <?php
-class Database{
+
+class Database
+{
     // 静态用self 非静态用this
-    //本地模式
-    // private static $dsn = "mysql:host=127.0.0.1;port=3306;dbname=myweb";
-    // private static $userName = "myweb";
-    // private static $userPassword = "my_web";
     //服务器模式
-    private static $dsn = "mysql:host=127.0.0.1;port=3306;dbname=item_forye_top";
-    private static $userName = "item_forye_top";
-    private static $userPassword = "asJwbkb8fLj326i6";
+    private static string $dsn;
+    private static string $userName;
+    private static string $userPassword;
 
-    public static $accountTable = 'account';
-    public static $itemTable = 'item';
-    public static $logTable = 'log';
-    public static $operationTable = 'operation';
-    public static $ipTable = 'ip';
+    public static string $accountTable = 'account';
+    public static string $itemTable = 'item';
+    public static string $logTable = 'log';
+    public static string $operationTable = 'operation';
 
-    private static $database;
+    private static PDO $database;
+
+    // 静态构造函数初始化数据库连接信息
+    private static function init()
+    {
+        $host = getenv('MYSQL_HOST') ?: "127.0.0.1";
+        $port = getenv('MYSQL_PORT') ?: "3306";
+        $database = getenv('MYSQL_DATABASE') ?: "nss_item_manage";
+        self::$dsn = 'mysql:host='.$host.';port='.$port.';dbname='.$database;
+        self::$userName = getenv('MYSQL_USERNAME') ?: 'nss_item_manage';
+        self::$userPassword = getenv('MYSQL_PASSWORD') ?: 'mD3FaxpiaAtBMNCZ';
+    }
+
     // 直接执行SQL指令 // 需要防止sql注入
-    public static function execute($command,...$content){
+    public static function execute($command, ...$content)
+    {
         // 构造预处理命令
         $stmt = self::$database->prepare($command);
         // 写入参数
-        for($i=1,$j=count($content);$i<=$j;$i++){
-            $stmt->bindValue($i,$content[$i-1]);
+        for ($i = 1, $j = count($content); $i <= $j; $i++) {
+            $stmt->bindValue($i, $content[$i - 1]);
         }
         // 执行成功
-        if($stmt->execute()){
+        if ($stmt->execute()) {
             return $stmt;
-        }else{
+        } else {
             exit(json_encode([
-                'opCode'=>321,
-                'message'=>'数据获取异常'
+                'opCode' => 321,
+                'message' => '数据获取异常'
             ]));//数据库执行异常代码
         }
     }
+
     // 连接数据库
-    public static function connect(){
-        self::$database = new PDO(self::$dsn,self::$userName,self::$userPassword);
-        if(self::$database->errorCode()){//是否连接数据库成功
+    public static function connect()
+    {
+        self::init();
+        self::$database = new PDO(self::$dsn, self::$userName, self::$userPassword);
+        if (self::$database->errorCode()) {//是否连接数据库成功
             exit(json_encode([
-                'opCode'=>320,
-                'message'=>'数据库异常'
+                'opCode' => 320,
+                'message' => '数据库异常'
             ]));//数据库异常代码
-        }else return true;
+        } else return true;
     }
+
     // 返回最后操作的id
-    public static function getLastId(){
+    public static function getLastId()
+    {
         return self::$database->lastInsertId();
     }
 }
+
 //创建数据库
 Database::connect();
